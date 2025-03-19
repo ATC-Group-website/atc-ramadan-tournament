0847973070af7167361fa143f3fdb324f88051c3
Moustafa-Ashraf04 <moustafa.ashraf277@gmail.com>
2025-03-19 10:39:44 +0200

    day 5 update

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 4500,
+  },
   plugins: [tailwindcss()],
 });
